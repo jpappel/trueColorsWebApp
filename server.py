d2c2e83bd48fd179a51026613d11b93a68de8ccd
Michael Romero <romerom@moravian.edu[core]>
2024-06-23 15:45:59 -0400

    Changed the redirect_uri to be taken from the .env, because it varies from the local project to the actual webapps version, so this allows us to have the same code and just different values in the .env

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, 'client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email', 'openid'],
-    redirect_uri='http://127.0.0.1:8000/callback')
+    redirect_uri= os.getenv('REDIRECT_URI'))
 
 def login_is_required(function):
     def wrapper(*args, **kwargs):
