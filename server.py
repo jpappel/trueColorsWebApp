e0e93529027a4cfe81e6ae061dfe21f867714d00
Michael Romero <romerom@moravian.edu[core]>
2024-07-01 20:17:22 -0400

    Added a time.sleep(1) before the token stuff

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from google.auth.transport.requests import Request
 from google.oauth2 import id_token
 import google.auth.transport.requests
 import pip._vendor.cachecontrol as cachecontrol  # Import cachecontrol
+import time
 
 dotenv.load_dotenv()
 if not os.getenv('FLASK_SECRET_KEY'):
@@ -76,6 +77,8 @@ def callback():
     request_session = flow.authorized_session()
     cached_session = cachecontrol.CacheControl(request_session)  # Use cachecontrol
     token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    time.sleep(1)  # Sleep for 1 second
 
     id_info = id_token.verify_oauth2_token(
         id_token=credentials.id_token,
@@ -201,8 +204,11 @@ def store_result():
         SELECT * FROM responses 
         WHERE user_id = %s AND test_id = %s AND question_num = %s AND group_num = %s;
         """
+        
+        test_id = """SELECT test_id FROM response_flags WHERE user_id = %s;"""
+
         # UPDATE TEST ID TO NOT JUST BE 1 
-        cursor.execute(select_query, (id, 1, question_num, group_num)) # 1 is the test Id, update when we figure that out
+        cursor.execute(select_query, (id, test_id, question_num, group_num)) # 1 is the test Id, update when we figure that out
         existing_record = cursor.fetchone()
 
         if existing_record:
